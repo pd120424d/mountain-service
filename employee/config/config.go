88b138d1b1ba5cbf513d2956d72e0deaf4f42855
pd120424d <pd120424d@student.etf.bg.ac.rs>
2024-08-31 14:27:01 +0200

    Add permanent delete for employee db.

--- employee/config/config.go
+++ employee/config/config.go
@@ -1,8 +1,6 @@
 package config
 
 import (
-	"fmt"
-
 	"gorm.io/gorm"
 
 	"mountain-service/shared/config"
@@ -11,25 +9,8 @@ import (
 
 const (
 	ServerPort = "8082"
-	dbPort     = "5432"
-	dbName     = "employee_service"
 )
 
 func GetEmployeeDB(log utils.Logger, connString string) *gorm.DB {
 	return config.GetDbConnection(log, connString)
-}
-
-func CreateEmployeeDB(log utils.Logger, connString string) {
-	db := config.GetDbConnection(log, connString)
-
-	cmd := fmt.Sprintf("CREATE DATABASE %s", dbName)
-	tx := db.Exec(cmd)
-	errDbExist := fmt.Sprintf("ERROR: database \"%s\" already exists (SQLSTATE 42P04)", dbName)
-	if tx.Error != nil && tx.Error.Error() != errDbExist {
-		log.Fatalf("failed to create '%v' database: %s", dbName, tx.Error.Error())
-	} else if tx.Error != nil {
-		log.Warnf("'%s' database already exists", dbName)
-	} else {
-		log.Infof("'%v' database created successfully", dbName)
-	}
 }
--- employee/internal/repositories/employee_repository.go
+++ employee/internal/repositories/employee_repository.go
@@ -4,8 +4,6 @@ package repositories
 
 import (
 	"gorm.io/gorm"
-	"time"
-
 	"mountain-service/employee/internal/model"
 	"mountain-service/shared/utils"
 )
@@ -45,6 +43,7 @@ func (r *employeeRepository) GetAll() ([]model.Employee, error) {
 	return employees, err
 }
 
+// GetEmployeeByID returns employee by its id or error if it cannot be found.
 func (r *employeeRepository) GetEmployeeByID(id string, employee *model.Employee) error {
 	return r.db.First(employee, "id = ?", id).Error
 }
@@ -65,20 +64,36 @@ func (r *employeeRepository) UpdateEmployee(employee *model.Employee) error {
 }
 
 // Delete marks the employee record as deleted by setting the deleted_at timestamp.
-func (r *employeeRepository) Delete(employeeID uint) error {
-	// First, check if the employee is already soft-deleted
+func (r *employeeRepository) Delete(id uint) error {
+	// Start by fetching the employee to ensure it exists
 	var employee model.Employee
-	err := r.db.Select("deleted_at").First(&employee, employeeID).Error
-	if err != nil {
-		// Return error if the employee is not found or any other issue occurs
+	if err := r.db.First(&employee, id).Error; err != nil {
 		return err
 	}
 
-	if employee.DeletedAt.Valid {
-		// Return an error if the employee is already soft-deleted
-		return gorm.ErrRecordNotFound
+	// Permanently delete the employee
+	if err := r.db.Unscoped().Delete(&employee).Error; err != nil {
+		return err
 	}
 
-	// If not already soft-deleted, mark the employee as deleted
-	return r.db.Model(&model.Employee{}).Where("id = ?", employeeID).Update("deleted_at", time.Now()).Error
+	return nil
 }
+
+// Delete marks the employee record as deleted by setting the deleted_at timestamp.
+//func (r *employeeRepository) Delete(employeeID uint) error {
+//	// First, check if the employee is already soft-deleted
+//	var employee model.Employee
+//	err := r.db.Select("deleted_at").First(&employee, employeeID).Error
+//	if err != nil {
+//		// Return error if the employee is not found or any other issue occurs
+//		return err
+//	}
+//
+//	if employee.DeletedAt.Valid {
+//		// Return an error if the employee is already soft-deleted
+//		return gorm.ErrRecordNotFound
+//	}
+//
+//	// If not already soft-deleted, mark the employee as deleted
+//	return r.db.Model(&model.Employee{}).Where("id = ?", employeeID).Update("deleted_at", time.Now()).Error
+//}
