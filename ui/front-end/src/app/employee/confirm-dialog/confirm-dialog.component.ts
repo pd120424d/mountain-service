d0c784fe00ce0b3af8ac559d299b4d0e305c738a
pd120424d <pd120424d@student.etf.bg.ac.rs>
2024-12-25 14:58:12 +0100

    Add employees confirm dialog in angular.

---
+++ ui/front-end/src/app/employee/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.css'],
+  standalone: true,
+  imports: [MatDialogModule, CommonModule],
+})
+export class ConfirmDialogComponent {
+  constructor(public dialogRef: MatDialogRef<ConfirmDialogComponent>) {}
+
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  onCancel(): void {
+    this.dialogRef.close(false);
+  }
+}
