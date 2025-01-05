d8989ba1528520764fbfbd7449ef32f02f79d321
artyom <pawla93slash@gmail.com>
2025-01-05 11:53:21 +0100

    Improve employee data service. Code cleanup.

--- ui/front-end/README.md
+++ ui/front-end/README.md
@@ -10,6 +10,10 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
+## Production server
+
+Run `ng build --prod` for a prod server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+
 ## Code scaffolding
 
 Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
--- ui/front-end/src/app/employee/employee.service.ts
+++ ui/front-end/src/app/employee/employee.service.ts
@@ -1,35 +1,54 @@
 // src/app/employee/employee.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';  // Import HttpClient
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 import { Employee } from './employee.model';
+import { environment } from '../../environments/environment'; // Import environment variables
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private apiUrl = 'http://localhost:8082/api/v1/employees'; // Replace with your actual API URL
+  private apiUrl = `${environment.apiUrl}/employees`;
+
+  // private apiUrl = 'http://localhost:8082/api/v1/employees'; // Replace with your actual API URL
 
   constructor(private http: HttpClient) { }  // Inject HttpClient
 
   // Example methods
   getEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(this.apiUrl);
+    return this.http.get<Employee[]>(this.apiUrl).pipe(
+      catchError(this.handleError)
+    );;
   }
 
   getEmployeeById(id: number): Observable<Employee> {
-    return this.http.get<Employee>(`${this.apiUrl}/${id}`);
+    return this.http.get<Employee>(`${this.apiUrl}/${id}`).pipe(
+      catchError(this.handleError)
+    );
   }
 
   addEmployee(employee: Employee): Observable<Employee> {
-    return this.http.post<Employee>(this.apiUrl, employee);
+    return this.http.post<Employee>(this.apiUrl, employee).pipe(
+      catchError(this.handleError)
+    );
   }
 
   updateEmployee(id: number, employee: Employee): Observable<Employee> {
-    return this.http.put<Employee>(`${this.apiUrl}/${id}`, employee);
+    return this.http.put<Employee>(`${this.apiUrl}/${id}`, employee).pipe(
+      catchError(this.handleError)
+    );
   }
 
   deleteEmployee(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/${id}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred:', error);
+    return throwError(() => new Error('Something went wrong; please try again later.'));
   }
 }
