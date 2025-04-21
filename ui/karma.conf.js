d81b2e136a216ce8d7e2cf31d85c06dcdaa2f05e
Душан Павловић <pd120424d@student.etf.bg.ac.rs>
2025-04-21 13:28:14 +0200

    Add karma.conf.js and ignore test coverage files.

---
+++ ui/karma.conf.js
@@ -0,0 +1,35 @@
+module.exports = function (config) {
+    config.set({
+      basePath: '',
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-jasmine-html-reporter'),
+        require('karma-coverage'),
+        require('@angular-devkit/build-angular/plugins/karma')
+      ],
+      client: {
+        jasmine: {},
+        clearContext: false // leave Jasmine Spec Runner output visible in browser
+      },
+      coverageReporter: {
+        dir: require('path').join(__dirname, './coverage/ui'),
+        subdir: '.',
+        reporters: [
+          { type: 'html' },
+          { type: 'text-summary' },
+          { type: 'json-summary' } // ✅ important for test-cover-ui.sh
+        ]
+      },
+      reporters: ['progress', 'kjhtml', 'coverage'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['Chrome'],
+      singleRun: false,
+      restartOnFileChange: true
+    });
+  };
+  
