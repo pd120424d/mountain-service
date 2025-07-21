dc2c5bdcc80a929d95eee2471c8ab3a7698b6fe0
Душан Павловић <pd120424d@student.etf.bg.ac.rs>
2025-07-21 09:11:50 +0200

    Make go home a link instead of a button on the login page.

--- ui/src/app/login/login.component.spec.ts
+++ ui/src/app/login/login.component.spec.ts
@@ -2,25 +2,54 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { sharedTestingProviders } from '../test-utils/shared-test-imports';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['login']);
+
     TestBed.configureTestingModule({
       imports: [LoginComponent],
-      providers: [...sharedTestingProviders, { provide: ActivatedRoute, useValue: { queryParams: of({}) } }]
+      providers: [
+        ...sharedTestingProviders,
+        { provide: ActivatedRoute, useValue: { queryParams: of({}) } },
+        { provide: AuthService, useValue: authServiceSpy }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show error message on 401 login failure', () => {
+    const error = { status: 401 };
+    authService.login.and.returnValue(throwError(() => error));
+    component.credentials = { username: 'test', password: 'wrong' };
+
+    component.onLogin();
+
+    expect(component.loginError).toBe(true);
+  });
+
+  it('should reset error message on new login attempt', () => {
+    component.loginError = true;
+    authService.login.and.returnValue(of({ token: 'fake-token' }));
+    component.credentials = { username: 'test', password: 'correct' };
+
+    component.onLogin();
+
+    expect(component.loginError).toBe(false);
   });
 });
--- ui/src/app/login/login.component.ts
+++ ui/src/app/login/login.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 
 @Component({
@@ -16,6 +18,7 @@ import { CommonModule } from '@angular/common';
 export class LoginComponent {
   credentials = { username: '', password: '' };
   sessionExpired = false;
+  loginError = false;
 
   constructor(private route: ActivatedRoute, private authService: AuthService, private router: Router, private translate: TranslateService) {
     this.translate.setDefaultLang('sr-cyr')
@@ -28,8 +31,18 @@ export class LoginComponent {
   }
 
   onLogin(): void {
-    this.authService.login(this.credentials).subscribe(() => {
-      this.router.navigate(['/']);
+    this.loginError = false;
+    this.authService.login(this.credentials).pipe(
+      catchError((error) => {
+        if (error.status === 401) {
+          this.loginError = true;
+        }
+        return of(null);
+      })
+    ).subscribe((response) => {
+      if (response) {
+        this.router.navigate(['/']);
+      }
     });
   }
 
